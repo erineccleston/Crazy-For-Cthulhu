84f255751bcbf3051d97d906b555e0bbe9546f6c
Erin <connoreccleston@gmail.com>
2018-02-09 19:47:22 -0700

    Added dialogue node scripts.

---
+++ Assets/Editor/DialogueNodeAsset.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DialogueNodeAsset
+{
+    [MenuItem("Assets/Create Dialogue Node")]
+    public static void CreateAsset()
+    {
+        ScriptableObjectUtility.CreateAsset<DialogueNode>();
+    }
+}
---
+++ Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DialogueNode : ScriptableObject
+{
+    [TextArea]
+    public string Dialogue;
+    public float SanityChange;
+    public Link[] Choices;
+
+    [System.Serializable]
+    public struct Link
+    {
+        [TextArea]
+        public string Response;
+        [Range(0, 100)]
+        public float SanityLowest;
+        [Range(0, 100)]
+        public float SanityHighest;
+        public DialogueNode Next;
+    }
+}
