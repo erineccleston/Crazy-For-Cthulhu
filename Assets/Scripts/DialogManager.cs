3a2e6abfefdbbada9f2ba4896d8ff240c5ae2f5b
Papapopp <32002023+Papapopp@users.noreply.github.com>
2018-02-10 00:28:38 -0700

    midnight oil

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -16,6 +16,9 @@ public class DialogManager : MonoBehaviour {
     GameObject opt3;
     GameObject opt4;
 
+    public DialogueNode StartNode;
+    private DialogueNode CurrentNode;
+
 	// Use this for initialization
 	void Start () {
         sentences = new Queue<string>();
@@ -26,29 +29,33 @@ public class DialogManager : MonoBehaviour {
          opt2 = GameObject.Find("option2");
          opt3 = GameObject.Find("option3");
          opt4 = GameObject.Find("option4");
+
+        CurrentNode = StartNode;
+        beginConversation();// CurrentNode);
 	}
 
-    public void beginConversation(DialogueNode node)
+    public void beginConversation()//DialogueNode node)
     {
         Debug.Log("starting conversation");
         //shows the date's words and name
         dateName.SetActive(true);
         dateWords.SetActive(true);
 
-        dateName.GetComponent<Text>().text = node.Name;
-        string[] lines = node.Dialogue.Split('\n');
+        dateName.GetComponent<Text>().text = CurrentNode.Name;
+        string[] lines = CurrentNode.Dialogue.Split('\n');
         foreach(string line in lines)
         {
             sentences.Enqueue(line);
         }
 
+        goToNextSentence();// node);
     }
 
-    public void goToNextSentence(DialogueNode node)
+    public void goToNextSentence()//DialogueNode node)
     {
         if(sentences.Count == 0)
         {
-            presentChoice(node);
+            presentChoice();// CurrentNode);
         } else
         {
             displaySentence(sentences.Dequeue()); 
@@ -60,28 +67,78 @@ public class DialogManager : MonoBehaviour {
         dateWords.GetComponent<Text>().text = currentLine;
     }
 
-    private void presentChoice(DialogueNode node)
+    public void presentChoice()//DialogueNode node)
     {
         //hides the date's words and name
         dateName.SetActive(false);
         dateWords.SetActive(false);
 
-        //shows your name and choices
+        //shows your name and choices, diables invalid choices.
         yourName.SetActive(true);
-        for(int i = 0; i < node.Choices.Length; i++)
+        for(int i = 0; i < CurrentNode.Choices.Length; i++)
         {
             if (i == 0)
-                opt1.GetComponent<Text>().text = node.Choices[i].Response;
+            {
+                opt1.GetComponent<Text>().text = CurrentNode.Choices[i].Response;
                 opt1.SetActive(true);
-            if (i == 2)
-                opt2.GetComponent<Text>().text = node.Choices[i].Response;
+                if (sanity_bar.sanity < CurrentNode.Choices[i].SanityHighest && sanity_bar.sanity > CurrentNode.Choices[i].SanityLowest)
+                {
+                    opt1.GetComponent<Button>().interactable = true;
+                }
+                else
+                {
+                    opt1.GetComponent<Button>().interactable = false;
+                }
+            }
+
+            if (i == 1)
+            {
+                opt2.GetComponent<Text>().text = CurrentNode.Choices[i].Response;
                 opt2.SetActive(true);
+                if (sanity_bar.sanity < CurrentNode.Choices[i].SanityHighest && sanity_bar.sanity > CurrentNode.Choices[i].SanityLowest)
+                {
+                    opt2.GetComponent<Button>().interactable = true;
+                } else
+                {
+                    opt2.GetComponent<Button>().interactable = false;
+                }
+                
+            }
+
             if (i == 2)
-                opt3.GetComponent<Text>().text = node.Choices[i].Response;
+            {
+                opt3.GetComponent<Text>().text = CurrentNode.Choices[i].Response;
                 opt3.SetActive(true);
+                if (sanity_bar.sanity < CurrentNode.Choices[i].SanityHighest && sanity_bar.sanity > CurrentNode.Choices[i].SanityLowest)
+                {
+                    opt3.GetComponent<Button>().interactable = true;
+                }
+                else
+                {
+                    opt3.GetComponent<Button>().interactable = false;
+                }
+            }
+
             if (i == 3)
-                opt4.GetComponent<Text>().text = node.Choices[i].Response;
+            {
+                opt4.GetComponent<Text>().text = CurrentNode.Choices[i].Response;
                 opt4.SetActive(true);
+                if (sanity_bar.sanity < CurrentNode.Choices[i].SanityHighest && sanity_bar.sanity > CurrentNode.Choices[i].SanityLowest)
+                {
+                    opt4.GetComponent<Button>().interactable = true;
+                }
+                else
+                {
+                    opt4.GetComponent<Button>().interactable = false;
+                }
+            }
         }
+    }
+
+    public void registerChoice(int choice)
+    {
+        sanity_bar.updateSanity
+        CurrentNode = CurrentNode.Choices[choice].Next;
+        beginConversation();
     }
 }
