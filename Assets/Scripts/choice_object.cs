afe62c633d0f07636244105d37b5d0dacb5fba9c
Kindra <scrunnchy@gmail.com>
2018-02-09 19:22:12 -0700

    Added choice_object.cs to scripts

---
+++ Assets/Scripts/choice_object.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class choice_object : MonoBehaviour {
+
+    /// <summary>
+    /// Offsets sanity. 0 is completely sane, 100 is completely insane.
+    /// </summary>
+    int sanity_offset;
+
+    /// <summary>
+    /// Holds the text of the choice object.
+    /// </summary>
+    string text;
+
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	//// Update is called once per frame
+	//void Update () {
+		
+	//}
+}
