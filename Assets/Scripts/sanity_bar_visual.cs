2ba1ee08e0712ae1be21553cf1360b4ff78df437
Kindra <scrunnchy@gmail.com>
2018-02-09 22:23:36 -0700

    fixing merge issues
    deleted gameOver, plan to dismiss my changes

--- Assets/Scripts/sanity_bar_visual.cs
+++ Assets/Scripts/sanity_bar_visual.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 
 public class sanity_bar_visual : MonoBehaviour {
 
-	// Use this for initialization
-	//void Start () {
- //       GameObject sanityBar = GameObject.Find("SanitySlider");
- //       Image fillImage = sanityBar.GetComponent<Image>();
+    // Use this for initialization
+    void Start()
+    {
+        GameObject sanityBar = GameObject.Find("SanitySlider");
+        Image fillImage = sanityBar.gameObject.GetComponent<Image>();
 
- //       fillImage.transform.Translate(100f, 100f, 100f);
-	//}
-	
-	//// Update is called once per frame
-	//void Update () {
- //       GameObject sanityBar = GameObject.Find("SanitySlider");
- //       Image fillImage = sanityBar.GetComponent<Image>();
+        fillImage.transform.Translate(100f, 100f, 100f);
+    }
 
- //       fillImage.transform.Translate(100f, 100f, 100f);
- //   }
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject sanityBar = GameObject.Find("SanitySlider");
+        Image fillImage = sanityBar.gameObject.GetComponent<Image>();
+
+        fillImage.transform.Translate(100f, 100f, 100f);
+    }
 }
