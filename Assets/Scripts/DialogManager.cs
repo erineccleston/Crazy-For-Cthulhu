302e2a421969585c04f7b175df660f49c937802b
Papapopp <32002023+Papapopp@users.noreply.github.com>
2018-02-09 21:48:16 -0700

    not broken

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,11 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogManager : MonoBehaviour {
 
     
     public Queue<string> sentences;
+    GameObject dateName = GameObject.Find("dateName");
 
 	// Use this for initialization
 	void Start () {
@@ -13,11 +16,11 @@ public class DialogManager : MonoBehaviour {
 
 	}
 
-    public void beginConversation(DialogueNode)
+    public void beginConversation(DialogueNode node)
     {
         Debug.Log("starting conversation");
-        dateName.Text = 
-        string[] lines = dialog.Split('\n');
+        dateName.GetComponent<Text>().text = node.Name;
+        string[] lines = node.Dialogue.Split('\n');
         foreach (string line in lines)
         {
             sentences.Enqueue(line);
@@ -29,11 +32,20 @@ public class DialogManager : MonoBehaviour {
     {
         if(sentences.Count == 0)
         {
-            
+            presentChoice();
+        } else
+        {
+            displaySentence(sentences.Dequeue()); 
         }
     }
 
+    private void displaySentence(string v)
+    {
+        throw new NotImplementedException();
+    }
 
-	
-	
+    private void presentChoice()
+    {
+        throw new NotImplementedException();
+    }
 }
