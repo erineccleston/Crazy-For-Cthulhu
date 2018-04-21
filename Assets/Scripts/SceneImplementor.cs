02600a966b968983c2cbb8692cb8cda326fd3e74
Alex <alexengineering1027@gmail.com>
2018-04-20 21:28:35 -0600

    New code
    Implemented empty scene implementor and some new events

---
+++ Assets/Scripts/SceneImplementor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneImplementorScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //functions to manager scene and
+    //cause proper toggles
+    //implemented here to keep things tidy
+    public static void IntroManager()
+    {
+
+    }
+
+    public static void MeetingManager()
+    {
+
+    }
+
+    public static void Date1Manager()
+    {
+
+    }
+
+    public static void Date2Manager()
+    {
+
+    }
+
+    public static void Date3Manager()
+    {
+
+    }
+
+    public static void FullInsanityManager()
+    {
+
+    }
+
+    public static void FullSanityManager()
+    {
+
+    }
+}
